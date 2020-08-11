de458b46fac8105aa31f6a81d6f0155fa5703bf9
ch-4ml <ark9659@gmail.com>
2020-08-11 09:47:27 +0000

    add applicaion test files

---
+++ application/affiliationTest.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const { Wallets, Gateway, X509WalletMixin } = require('fabric-network');
+const FabricCAServices = require('fabric-ca-client');
+const fs = require('fs');
+const path = require('path');
+const { AffiliationService } = require('fabric-ca-client');
+
+async function main() {
+  try {
+    // load the network configuration
+    const ccpPath = path.resolve(__dirname, '..', 'first-network', 'connection-org3.json');
+    const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+
+    // Create a new CA client for interacting with the CA.
+    const caURL = ccp.certificateAuthorities['ca.org3.example.com'].url;
+    const ca = new FabricCAServices(caURL);
+
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(process.cwd(), 'wallet');
+    const wallet = await Wallets.newFileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Check to see if we've already enrolled the admin user.
+    const adminIdentity = await wallet.get('admin3');
+    if (!adminIdentity) {
+        console.log('An identity for the admin user "admin3" does not exist in the wallet');
+        console.log('Run the enrollAdmin.js application before retrying');
+        return;
+    }
+
+    // build a user object for authenticating with the CA
+    const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
+    const adminUser = await provider.getUserContext(adminIdentity, 'admin3');
+    
+    const affiliationService = ca.newAffiliationService();
+
+    const result = await affiliationService.create({
+      req: {
+        name: "org3.department1"
+      },
+      registrar: {
+        req: {
+          name: "1"
+        }
+      }
+    });
+
+    console.log(result);
+    
+  } catch (error) {
+      console.error(`Failed to add affiliation: ${error}`);
+      process.exit(1);
+  }
+}
+
+main();
--- application/readMarble3.js
+++ application/readMarble3.js
@@ -40,7 +40,7 @@ async function main() {
     try {
         // Parse the connection profile. This would be the path to the file downloaded
         // from the IBM Blockchain Platform operational console.
-        const ccpPath = path.resolve(__dirname, '..', 'first-network', 'connection-org1.json');
+        const ccpPath = path.resolve(__dirname, '..', 'first-network', 'connection-org3.json');
         const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
 
         // Configure a wallet. This wallet must already be primed with an identity that
@@ -51,7 +51,7 @@ async function main() {
         // Create a new gateway, and connect to the gateway peer node(s). The identity
         // specified must already exist in the specified wallet.
         const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+        await gateway.connect(ccp, { wallet, identity: 'appUser3', discovery: { enabled: true, asLocalhost: true } });
 
         // Get the network channel that the smart contract is deployed to.
         const network = await gateway.getNetwork(channelid);
@@ -59,7 +59,7 @@ async function main() {
         // Get the smart contract from the network channel.
         const contract = network.getContract('marblesp');
 
-        const result = await contract.submitTransaction('readMarble', 'marble550');
+        const result = await contract.submitTransaction('readMarble', 'marble580');
         console.log(JSON.parse(result.toString()));
 
         await gateway.disconnect();
